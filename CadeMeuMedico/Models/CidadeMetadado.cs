d8632c0b556e48b946ec22420faa9e68c35ce92b
julio.souza <juliogiacobbo@gmail.com>
2016-09-26 14:35:31 -0300

    CRUD

---
+++ CadeMeuMedico/Models/CidadeMetadado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace CadeMeuMedico.Models
+{
+    [MetadataType(typeof(CidadeMetadado))]
+    public partial class Cidade
+    {
+    }
+    public class CidadeMetadado
+    {
+        [Required(ErrorMessage = "Obrigatório informar o Nome")]
+        [StringLength(100, ErrorMessage = "O Nome deve possuir no máximo 100 caracteres")]
+        public string Nome { get; set; }
+    }
+}
