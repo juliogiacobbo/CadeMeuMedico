debec8e9bb1d27a94a88bbf024a87183c2cbdce8
julio.souza <juliogiacobbo@gmail.com>
2016-10-04 09:09:16 -0300

    criação do projeto com os scripts do banco

---
+++ Database/BannersPublicitarios.sql
@@ -0,0 +1,10 @@
+﻿
+CREATE TABLE BannersPublicitarios
+(
+IDBanner BIGINT IDENTITY NOT NULL,
+TituloCampanha VARCHAR(60) NOT NULL,
+BannerCampanha VARCHAR(200) NOT NULL,
+LinkBanner VARCHAR(200) NULL,
+PRIMARY KEY(IDBanner)
+);
+
---
+++ Database/Insert.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO BannersPublicitarios
+(TituloCampanha, BannerCampanha, LinkBanner) VALUES
+('Campanha Conio', 'logo-conio-cademeumedico.png','http://conio.com.br')
+
+INSERT INTO BannersPublicitarios
+(TituloCampanha, BannerCampanha, LinkBanner) VALUES
+('Campanha Casa do Código', 'banner-cdc-cademeumedico.png', 'http://casadocodigo.com.br')
