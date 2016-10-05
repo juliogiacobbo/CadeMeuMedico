de795361dfe296c7e9d4c4bbbde75e176628fb2c
juliogiacobbo <juliogiacobbo@gmail.com>
2016-10-05 16:04:26 -0300

    Filtro de Ação OK

--- CadeMeuMedico/Repositorios/RepositorioUsuarios.cs
+++ CadeMeuMedico/Repositorios/RepositorioUsuarios.cs
@@ -1,13 +1,13 @@
 ﻿using CadeMeuMedico.Models;
 using System;
 using System.Linq;
+using System.Web;
 using System.Web.Security;
 
 namespace CadeMeuMedico.Repositorios
 {
     public class RepositorioUsuarios
     {
-
         public static bool AutenticarUsuario(string Login, string Senha)
         {
             var SenhaCriptografada = FormsAuthentication.HashPasswordForStoringInConfigFile(Senha, "sha1");
@@ -31,6 +31,42 @@ namespace CadeMeuMedico.Repositorios
             catch (Exception)
             {
                 return false;
+            }
+        }
+
+        //Os nomes de ambos osmétodos apresentados pela listagem 7 já dão ideia de suas
+        //funções no contexto da aplicação.O primeiro tenta recuperar o registro único de
+        //usuário baseado no código proveniente do parâmetro do mesmo.O segundo toma
+        //como base o usuário recuperado pelo primeiro método e verifica se o usuário já se
+        //encontra autenticado no sistema.Simples e funcional.
+        public static Usuario RecuperaUsuarioPorID(long IDUsuario)
+        {
+            try
+            {
+                using (EntidadesCadeMeuMedicoBD2 db = new EntidadesCadeMeuMedicoBD2())
+                {
+                    var Usuario = db.Usuario.Where(u => u.IDUsuario == IDUsuario).SingleOrDefault();
+                    return Usuario;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public static Usuario VerificaSeOUsuarioEstaLogado()
+        {
+            var Usuario = HttpContext.Current.Request.Cookies["UserCookieAuthentication"];
+            if (Usuario == null)
+            {
+                return null;
+            }
+            else
+            {
+                long IDUsuario = Convert.ToInt64(RepositorioCriptografia.Descriptografar(Usuario.Values["IDUsuario"]));
+                var UsuarioRetornado = RecuperaUsuarioPorID(IDUsuario);
+                return UsuarioRetornado;
             }
         }
 
